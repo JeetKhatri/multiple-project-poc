6e6b36b5d372613ae1de6085b3658eb43cbec982
JeetKhatri <jeet.g.khatri@gmail.com>
2019-10-11 11:21:18 +0530

    portlet added

---
+++ multiple-project-poc-child1/src/main/java/com/opensource/techblog/springmvc/portlet1/ProductPortlet.java
@@ -0,0 +1,11 @@
+package com.opensource.techblog.springmvc.portlet1;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ProductPortlet
+ */
+public class ProductPortlet extends MVCPortlet {
+ 
+
+}
